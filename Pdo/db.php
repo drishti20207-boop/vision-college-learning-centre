c0c5788e3f6bd41c966dd5fda6fdee543841ad99
Drishti <drishti20207@gmail.com>
2026-08-31 10:00:41 +1200

    Add files via upload

---
+++ Pdo/db.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = "localhost";
+$dbname = "vision_college";
+$username = "root";
+$password = "";
+
+try {
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+
+    die("Database connection failed: " . $e->getMessage());
+
+}
+?>
